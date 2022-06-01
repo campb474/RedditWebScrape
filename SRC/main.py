856775f68279fd419baf19bf3cf963bbe3ef3477
campb474 <campbellmatt646@gmail.com>
2022-06-01 00:02:07 -0400

    redditScrape class

--- SRC/main.py
+++ SRC/main.py
@@ -2,27 +2,36 @@ import requests
 from dotenv import load_dotenv
 import os
 
-# Getting secure information form environment file
-load_dotenv()
-PASSWORD = os.environ.get("password")
-USERNAME = os.environ.get("username")
-CLIENT_ID = os.environ.get("CLIENT_ID")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+class redditScrape:
 
-# Getting authorization token
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-data = {
-    'grant_type': 'password',
-    'username': USERNAME,
-    'password': PASSWORD,
-}
-headers = {'User-Agent': 'MyBot/0.0.1'}
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
-TOKEN = res.json()['access_token']
-headers['authorization'] = f'bearer {TOKEN}'
+    # Initializes environemnt variables and reddit api connection
+    def __init__(self):
+        # Getting secure information form environment file
+        load_dotenv()
+        self.PASSWORD = os.environ.get("password")
+        self.USERNAME = os.environ.get("username")
+        self.CLIENT_ID = os.environ.get("CLIENT_ID")
+        self.SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# Accessing Reddit API
-# <Response [403]> -> indicates error
-# <Response [200]> -> indicates that everything is fine
-print(requests.get("https://oauth.reddit.com/api/v1/me", headers=headers))
+        # Getting authorization token
+        auth = requests.auth.HTTPBasicAuth(self.CLIENT_ID, self.SECRET_KEY)
+        data = {
+            'grant_type': 'password',
+            'username': self.USERNAME,
+            'password': self.PASSWORD,
+        }
+        self.headers = {'User-Agent': 'MyBot/0.0.1'}
+        res = requests.post('https://www.reddit.com/api/v1/access_token',
+                            auth=auth, data=data, headers=self.headers)
+        self.TOKEN = res.json()['access_token']
+        self.headers['authorization'] = f'bearer {self.TOKEN}'
+
+        # Accessing Reddit API
+        # <Response [403]> -> indicates error
+        # <Response [200]> -> indicates that everything is fine
+        print(requests.get("https://oauth.reddit.com/api/v1/me", headers=self.headers))
+
+def main():
+    reddit = redditScrape()
+
+main()
